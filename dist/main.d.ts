e3aa3f3f89fc950c5455f6d222c2ff1ae54634fb
ussaohelcim <57050328+ussaohelcim@users.noreply.github.com>
2022-06-28 19:16:55 -0300

    added overboard functions

--- dist/main.d.ts
+++ dist/main.d.ts
@@ -1,8 +1,22 @@
-import { IThread, INewPost } from "jschan-api-types";
+import { IThread, INewPost, IBoardList } from "jschan-api-types";
 export declare namespace jschan {
     class api {
         url: string;
+        /**
+         * Creates a new api sdk.
+         * Please remove the last '/' from the url.
+         * 	Ex:
+         * 	  https://ptchan.org   < RIGHT
+         * 	  https://fatchan.org/  < WRONG
+         * ```js
+         * const api = new jschan.api("https://ptchan.org")
+         * ```
+         * @param root the root path of a jschan instance
+         */
         constructor(root: string);
+        /**
+         * Returns a thread with all replies.
+         */
         getThread(board_id: string, postId: number): Promise<IThread>;
         /**
          * Create a new thread or reply a thread.
@@ -10,9 +24,38 @@ export declare namespace jschan {
          * @param reply Reply object, with message, file, email etc...
          * @param postId post number, such as 55555, to use as referer. If undefined, the board will be used as referer.
          */
-        postThread(board_id: string, reply: INewPost, postId?: number): Promise<void>;
+        postThread(board_id: string, reply: INewPost, postId?: number): Promise<number>;
+        /**
+         * Returns a list of boards, max 30 per page. Unlisted boards are not included in search results. Also return the current page and maximum page with the current search and sites parameters.
+         * @returns
+         */
+        getBoardList(): Promise<IBoardList>;
+        /**
+         * Returns a list of all threads on a board.
+         * @param board board id
+         * @returns a list of all threads on a board
+         */
+        getBoardCatalog(board: string): Promise<IThread[]>;
+        /**
+         * Returns a string with the path to the thumbs directory.
+         * @returns website.com/file/thumb/
+         */
         getThumbPath(): string;
+        /**
+         * Returns a string with the path to the files directory.
+         * @returns website.com/file/
+         */
         getFilesPath(): string;
-        get(url: string): Promise<any>;
+        /**
+         * Returns a list of threads without replies from multiple boards. Similar to board catalog pages.
+         * @returns list of threads.
+         */
+        getOverboardCatalog(): Promise<IThread[]>;
+        /**
+         * Returns a list of threads with preview replies from multiple boards. Similar to board index pages.
+         * @returns list of threads with preview replies.
+         */
+        getOverboardIndex(): Promise<IThread[]>;
+        private get;
     }
 }
--- dist/main.js
+++ dist/main.js
@@ -9,9 +9,23 @@ const path_1 = __importDefault(require("path"));
 var jschan;
 (function (jschan) {
     class api {
+        /**
+         * Creates a new api sdk.
+         * Please remove the last '/' from the url.
+         * 	Ex:
+         * 	  https://ptchan.org   < RIGHT
+         * 	  https://fatchan.org/  < WRONG
+         * ```js
+         * const api = new jschan.api("https://ptchan.org")
+         * ```
+         * @param root the root path of a jschan instance
+         */
         constructor(root) {
             this.url = root;
         }
+        /**
+         * Returns a thread with all replies.
+         */
         async getThread(board_id, postId) {
             const t = await this.get(`${this.url}/${board_id}/thread/${postId}.json`);
             return t;
@@ -23,6 +37,7 @@ var jschan;
          * @param postId post number, such as 55555, to use as referer. If undefined, the board will be used as referer.
          */
         async postThread(board_id, reply, postId) {
+            let resCode = 0;
             await axios_1.default.post(`${this.url}/forms/board/${board_id}/post`, reply, { method: "POST", headers: {
                     "User-Agent": "jschan-api-sdk",
                     "Referer": postId ? path_1.default.normalize(`${this.url}/${board_id}/thread/${postId}.html`) : path_1.default.normalize(`${this.url}/${board_id}/index.html`),
@@ -30,17 +45,60 @@ var jschan;
                     "Content-Type": "multipart/form-data"
                 } })
                 .then((res) => {
-                console.log(res.status, res.statusText);
-                console.log(``);
+                resCode = res.status;
             }).catch((err) => {
                 throw new Error("Something went wrong");
             });
+            return resCode;
         }
+        /**
+         * Returns a list of boards, max 30 per page. Unlisted boards are not included in search results. Also return the current page and maximum page with the current search and sites parameters.
+         * @returns
+         */
+        async getBoardList() {
+            //TODO implement queries
+            const boards = await this.get(`${this.url}/boards.json`);
+            return boards;
+        }
+        /**
+         * Returns a list of all threads on a board.
+         * @param board board id
+         * @returns a list of all threads on a board
+         */
+        async getBoardCatalog(board) {
+            let catalog = await this.get(`${this.url}/${board}/catalog.json`);
+            return catalog;
+        }
+        /**
+         * Returns a string with the path to the thumbs directory.
+         * @returns website.com/file/thumb/
+         */
         getThumbPath() {
-            return path_1.default.normalize(`${this.getFilesPath()}/thumb/`);
+            return `${this.getFilesPath()}/thumb/`;
         }
+        /**
+         * Returns a string with the path to the files directory.
+         * @returns website.com/file/
+         */
         getFilesPath() {
-            return path_1.default.normalize(`${this.url}/file/`);
+            return `${this.url}/file/`;
+        }
+        /**
+         * Returns a list of threads without replies from multiple boards. Similar to board catalog pages.
+         * @returns list of threads.
+         */
+        async getOverboardCatalog() {
+            const catalog = await this.get(`${this.url}/catalog.json`);
+            return catalog;
+        }
+        /**
+         * Returns a list of threads with preview replies from multiple boards. Similar to board index pages.
+         * @returns list of threads with preview replies.
+         */
+        async getOverboardIndex() {
+            //
+            const catalog = await this.get(`${this.url}/overboard.json`);
+            return catalog;
         }
         async get(url) {
             const res = (await axios_1.default.get(url, { headers: {
